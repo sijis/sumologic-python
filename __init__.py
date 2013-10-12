f5fa0590b3bb7ed87eb6d3e4bf98b8dc7f4fd3fa
Sijis Aviles <saviles@gogoair.com>
2013-10-11 20:01:16 -0500

    added search functionality to Client class

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-from client import *
+from sumologic.client import *
--- client.py
+++ client.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python -tt
 
+import requests
+import datetime
+
 
 class Client(object):
 
     def __init__(self, auth, **kwargs):
-
         self.auth = auth
         self.protocol = kwargs.get('protocol', 'https')
         self.domain = kwargs.get('domain', 'api.sumologic.com')
@@ -12,14 +14,45 @@ class Client(object):
         api_path = '%s/logs/search' % self.api
 
         self.url = '%s://%s%s' % (self.protocol, self.domain, api_path)
-        self.debug = kwargs.get('debug', False)
-
-        self.set_debug(self.debug)
+        self.debug_mode = kwargs.get('debug', False)
 
     def set_debug(self, debug):
-        if debug:
+        self.debug_mode = debug
+
+    def debug(self, content=None):
+        if self.debug_mode:
             options = [
-                'auth', 'protocol', 'domain', 'api', 'url', 'debug'
+                'auth', 'protocol', 'domain', 'api', 'url', 'debug_mode'
             ]
             for option in options:
                 print '%s => %s' % (option, getattr(self, option))
+            print 'Content: %s ' % content
+
+    def search(self, criteria, **opts):
+
+        time_now = datetime.datetime.now().replace(second=0, microsecond=0)
+        right_now = time_now.isoformat()
+        minutes_ago = (time_now - datetime.timedelta(minutes=15)).isoformat()
+
+        formats = opts.get('formats', 'json')
+        timezone = opts.get('timezone', 'UTC')
+        time_from = opts.get('time_from', minutes_ago)
+        time_to = opts.get('time_to', right_now)
+
+        # setting up options
+        t_options = []
+        t_options.append('q=%s' % criteria)
+        t_options.append('format=%s' % formats)
+        t_options.append('tz=%s' % timezone)
+        t_options.append('from=%s' % time_from)
+        t_options.append('to=%s' % time_to)
+        options = '&'.join(t_options)
+
+        self.debug(options)
+
+        req = requests.get('%s?%s' % (self.url, options), auth=self.auth)
+        data = {}
+        data['data'] = req.json()
+        data['response'] = req.status_code
+        data['reason'] = req.reason
+        return data
