a0ebbd0576b7cd9ae68d14e4947c197135fe28ce
Sijis Aviles <saviles@gogoair.com>
2013-10-30 21:53:17 -0500

    removed search method to move to a separate class

--- client.py
+++ client.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python -tt
 
-import requests
-import datetime
-
-
 class Client(object):
 
     def __init__(self, auth, **kwargs):
@@ -11,8 +7,7 @@ class Client(object):
         self.protocol = kwargs.get('protocol', 'https')
         self.domain = kwargs.get('domain', 'api.sumologic.com')
         self.api = kwargs.get('api', '/api/v1')
-        api_path = '%s/logs/search' % self.api
-
+        api_path = '%s' % self.api
         self.url = '%s://%s%s' % (self.protocol, self.domain, api_path)
         self.debug_mode = kwargs.get('debug', False)
 
@@ -30,29 +25,8 @@ class Client(object):
             print 'Content: %s ' % content
             print '----------------------'
 
-    def search(self, criteria, **opts):
+    def get_url(self):
+        return self.url
 
-        time_now = datetime.datetime.now().replace(second=0, microsecond=0)
-        right_now = time_now.isoformat()
-        minutes_ago = (time_now - datetime.timedelta(minutes=15)).isoformat()
-
-        formats = opts.get('formats', 'json')
-        timezone = opts.get('timezone', 'UTC')
-        time_from = opts.get('time_from', minutes_ago)
-        time_to = opts.get('time_to', right_now)
-
-        # setting up options
-        t_options = []
-        t_options.append('q=%s' % criteria)
-        t_options.append('format=%s' % formats)
-        t_options.append('tz=%s' % timezone)
-        t_options.append('from=%s' % time_from)
-        t_options.append('to=%s' % time_to)
-        options = '&'.join(t_options)
-
-        req = requests.get('%s?%s' % (self.url, options), auth=self.auth)
-        data = {}
-        data['data'] = req.json()
-        data['response'] = req.status_code
-        data['reason'] = req.reason
-        return data
+    def get_auth(self):
+        return self.auth
