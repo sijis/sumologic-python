de14451427b23a63d4bc3c1b7af499797ae673c7
Sijis Aviles <sijis.aviles+github@gmail.com>
2014-01-01 22:43:31 -0600

    adding setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sumologic',
+    version = '0.1',
+    description='Python library for working with the Sumo Logic api.',
+    author='Sijis Aviles',
+    author_email='sijis.aviles@gmail.com',
+    packages = find_packages(),
+    install_requires = ['requests'],
+    keywords = "sumologic api python",
+    url = 'https://github.com/sijis/sumologic-python',
+)
+
